170ed05d9857dc0e499d787409c9306a19095061
duc15 <04phamduc@gmail.com>
2025-01-21 21:57:39 +0700

    update profile UI

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -17,12 +17,18 @@ class _ProfilePageState extends State<ProfilePage> {
       .child(FirebaseAuth.instance.currentUser!.uid)
       .child('ratings');
   String _ratings = "Loading...";
+  String _address = "Loading...";
+  String _dob = "Loading...";
+  bool isVerified = false;
+
   TextEditingController nameTextEditingController = TextEditingController();
   TextEditingController phoneTextEditingController = TextEditingController();
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController carTextEditingController = TextEditingController();
 
-  setDriverInfo() {
+  String currentDriverTotalTripsCompleted = "0";
+
+  void setDriverInfo() {
     setState(() {
       nameTextEditingController.text = driverName;
       phoneTextEditingController.text = driverPhone;
@@ -32,8 +38,6 @@ class _ProfilePageState extends State<ProfilePage> {
     });
   }
 
-  @override
-  String currentDriverTotalTripsCompleted = "";
   void _fetchRatings() async {
     DatabaseEvent event = await _databaseReference.once();
     setState(() {
@@ -41,24 +45,61 @@ class _ProfilePageState extends State<ProfilePage> {
     });
   }
 
-  getCurrentDriverTotalNumberOfTripsCompleted() async {
+  Future<void> _fetchAddress() async {
+    DatabaseReference addressRef = FirebaseDatabase.instance
+        .ref()
+        .child("drivers")
+        .child(FirebaseAuth.instance.currentUser!.uid)
+        .child("infoPersonal")
+        .child("address");
+
+    DatabaseEvent event = await addressRef.once();
+    setState(() {
+      _address =
+          event.snapshot.value?.toString() ?? "Chưa có thông tin địa chỉ";
+    });
+  }
+
+  Future<void> _fetchDOB() async {
+    DatabaseReference dobRef = FirebaseDatabase.instance
+        .ref()
+        .child("drivers")
+        .child(FirebaseAuth.instance.currentUser!.uid)
+        .child("infoPersonal")
+        .child("dob");
+
+    DatabaseEvent event = await dobRef.once();
+    setState(() {
+      _dob = event.snapshot.value?.toString() ?? "Chưa có thông tin ngày sinh";
+    });
+  }
+
+  void _fetchVerificationStatus() async {
+    DatabaseReference userRef = FirebaseDatabase.instance
+        .ref()
+        .child("drivers")
+        .child(FirebaseAuth.instance.currentUser!.uid);
+
+    DatabaseEvent event = await userRef.once();
+    setState(() {
+      isVerified = event.snapshot.child('isVerified').value as bool? ?? false;
+    });
+  }
+
+  void getCurrentDriverTotalNumberOfTripsCompleted() async {
     DatabaseReference tripRequestsRef =
         FirebaseDatabase.instance.ref().child("tripRequests");
 
     await tripRequestsRef.once().then((snap) async {
       if (snap.snapshot.value != null) {
         Map<dynamic, dynamic> allTripsMap = snap.snapshot.value as Map;
-        int allTripsLength = allTripsMap.length;
-
         List<String> tripsCompletedByCurrentDriver = [];
 
         allTripsMap.forEach((key, value) {
-          if (value["status"] != null) {
-            if (value["status"] == "ended") {
-              if (value["driverID"] == FirebaseAuth.instance.currentUser!.uid) {
-                tripsCompletedByCurrentDriver.add(key);
-              }
-            }
+          if (value["status"] != null &&
+              value["status"] == "ended" &&
+              value["driverID"] == FirebaseAuth.instance.currentUser!.uid) {
+            tripsCompletedByCurrentDriver.add(key);
           }
         });
 
@@ -72,180 +113,198 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     setDriverInfo();
     _fetchRatings();
+    _fetchAddress();
+    _fetchDOB();
+    _fetchVerificationStatus();
     getCurrentDriverTotalNumberOfTripsCompleted();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Thông tin cá nhân',
-                style: TextStyle(fontSize: 30),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              // Card hiển thị thông tin
-              Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                elevation: 5,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      // Ảnh đại diện và tên
-                      Row(
+      backgroundColor: Colors.blueGrey[50],
+      appBar: AppBar(
+        title: const Text('Thông tin cá nhân'),
+        backgroundColor: Colors.blueAccent,
+        elevation: 0,
+      ),
+      body: Stack(
+        children: [
+          // Wave background effect
+          Positioned.fill(
+            child: CustomPaint(
+              painter: WavePainter(),
+            ),
+          ),
+          // Content
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ListView(
+                children: [
+                  // Profile Picture and Name
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    elevation: 10,
+                    shadowColor: Colors.black.withOpacity(0.2),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
                         children: [
-                          CircleAvatar(
-                            radius: 40,
-                            backgroundImage: NetworkImage(
-                                driverPhoto), // Thay bằng ảnh từ driverPhoto
-                          ),
-                          const SizedBox(width: 16),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                          Row(
                             children: [
-                              Text(
-                                driverName, // Thay bằng driverName
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                              CircleAvatar(
+                                radius: 50,
+                                backgroundImage: NetworkImage(driverPhoto),
                               ),
-                              const SizedBox(height: 4),
-                              const Text(
-                                "Tài xế", // Thay bằng vai trò hoặc bất kỳ thông tin mô tả
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 14,
-                                ),
+                              const SizedBox(width: 16),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    driverName,
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 4),
+                                  const Text(
+                                    "Tài xế",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Column(
+                                children: [
+                                  Text(
+                                    currentDriverTotalTripsCompleted,
+                                    style: const TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.blueAccent,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 4),
+                                  const Text(
+                                    "Tổng chuyến đã đi",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    _ratings.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.blueAccent,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 4),
+                                  const Text(
+                                    "Đánh giá trung bình",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
                               ),
                             ],
                           ),
                         ],
                       ),
-                      const SizedBox(height: 16),
-                      // Các thông tin thống kê (Shifts)
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Column(
-                            children: [
-                              Text(
-                                currentDriverTotalTripsCompleted,
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              const SizedBox(height: 4),
-                              const Text(
-                                "Tổng chuyến đã đi",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                _ratings.toString(), // Thay bằng ratingDriver
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              const SizedBox(height: 4),
-                              const Text(
-                                "Đánh giá trung bình",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 16),
-                      const Divider(thickness: 1.2),
-                      const SizedBox(height: 16),
-                      // Các thông tin liên hệ
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // Info Details Section
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    elevation: 10,
+                    shadowColor: Colors.black.withOpacity(0.1),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
                         children: [
                           InfoRow(
                             icon: Icons.phone,
-                            text: driverPhone, // Thay bằng driverPhone
+                            text: driverPhone,
                           ),
                           const SizedBox(height: 12),
                           InfoRow(
                             icon: Icons.email,
                             text: FirebaseAuth.instance.currentUser!.email
-                                .toString(), // Thay bằng email
+                                .toString(),
                           ),
                           const SizedBox(height: 12),
-                          const InfoRow(
-                              icon: Icons.location_on,
-                              text:
-                                  "Dục Tú - Đông Anh - Hà Nội" // Thay bằng email
-                              ),
-                          SizedBox(height: 12),
                           InfoRow(
-                            icon: Icons.numbers,
-                            text: carNumber, // Địa chỉ
+                            icon: Icons.location_on,
+                            text: _address,
                           ),
-                          SizedBox(height: 12),
+                          const SizedBox(height: 12),
                           InfoRow(
-                              icon: Icons.apartment,
-                              text:
-                                  "$carModel - $carColor" // Thông tin thành phố
-                              ),
+                            icon: Icons.date_range,
+                            text: _dob,
+                          ),
+                          const SizedBox(height: 12),
+                          InfoRow(
+                            icon: Icons.car_crash,
+                            text: "$carModel - $carColor",
+                          ),
+                          const SizedBox(height: 12),
+                          InfoRow(
+                            icon: Icons.check_circle,
+                            text: isVerified ? "Đã xác thực" : "Chưa xác thực",
+                          ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 32),
-              // Nút Edit và Logout
-
-              ElevatedButton.icon(
-                onPressed: () {
-                  // Chức năng đăng xuất
-                  FirebaseAuth.instance.signOut();
-                },
-                icon: const Icon(Icons.logout),
-                label: const Text("Đăng xuất"),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  foregroundColor: Colors.white,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 32, vertical: 14),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+                  const SizedBox(height: 32),
+                  // Logout Button
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      FirebaseAuth.instance.signOut();
+                    },
+                    icon: const Icon(Icons.logout),
+                    label: const Text("Đăng xuất"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 32, vertical: 14),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -264,17 +323,44 @@ class InfoRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Icon(icon, color: Colors.blue),
+        Icon(icon, color: Colors.blueAccent, size: 24),
         const SizedBox(width: 12),
-        Text(
-          text,
-          style: const TextStyle(
-            fontSize: 16,
-            color: Colors.black,
+        Expanded(
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.black,
+            ),
+            overflow: TextOverflow.ellipsis,
           ),
         ),
       ],
     );
+  }
+}
+
+class WavePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.blueAccent.withOpacity(0.2)
+      ..style = PaintingStyle.fill;
+
+    Path path = Path();
+    path.lineTo(0, 0);
+    path.quadraticBezierTo(
+        size.width * 0.1, size.height * 0.8, size.width, 0.6);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
